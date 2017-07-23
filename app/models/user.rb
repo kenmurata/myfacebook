f5340649e63cedce8bf1739901080719054a4f2e
Ken Murata <k_murata@nttpc.co.jp>
2017-07-23 05:14:28 +0000

    DIVE20-2 image 051428

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,9 +28,12 @@ class User < ActiveRecord::Base
   end
   
   def self.find_for_facebook_oauth(auth, signed_in_resource = nil)
-    user = User.find_by(email: "#{auth.uid}-#{auth.provider}@example.com")
-    if  user == nil
-      user = User.find_by(provider: auth.provider, uid: auth.uid)
+    #user = User.find_by(email: "#{auth.uid}-#{auth.provider}@example.com")
+    user = User.find_by(email: auth.info.email) 
+    
+    if ! user
+      #user = User.find_by(provider: auth.provider, uid: auth.uid)
+      user = User.find_by(email: "#{auth.uid}-#{auth.provider}@example.com")
     end
     
     unless user
